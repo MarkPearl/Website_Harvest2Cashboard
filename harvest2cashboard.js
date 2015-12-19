32fc5142205f60e5c25d265e117a9f2773fd63ae
Mark Pearl <markpearl@gmail.com>
2015-12-19 09:38:16 +0200

    More

--- harvest2cashboard.js
+++ harvest2cashboard.js
@@ -3,5 +3,13 @@ angular.module('h2cApp', [])
         var h2c = this;
         h2c.doSomething = function() {
             alert('boo');
+            $http({
+                    method: 'GET',
+                    url: '/'
+            }).then(function successCallback(response) {
+                alert('success');
+            }, function errorCallback(response) {
+                alert('failed');
+            }
         };
     });
