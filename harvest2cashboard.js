a65f35feda9f0d02484837f6ab898f65d57a0978
Mark Pearl <markpearl@gmail.com>
2015-12-19 09:02:50 +0200

    add more meat to basic controller

---
+++ harvest2cashboard.js
@@ -0,0 +1,8 @@
+angular.module('h2cApp', [])
+    .controller('harvest2CashboardController', function() {
+        var h2c = this;
+        h2c.doSomething = function() {
+            alert('boo');
+        }
+    };
+});
