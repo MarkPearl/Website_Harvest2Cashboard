33f27f2377d61810b073ac80d4f7b0f71da4eb25
Mark Pearl <markpearl@gmail.com>
2015-12-19 12:13:14 +0200

    More

--- harvest2cashboard.js
+++ harvest2cashboard.js
@@ -1,13 +1,11 @@
 angular.module('h2cApp', [])
     .controller('harvest2CashboardController', function($http) {
         var h2c = this;
-        $http.defaults.headers.common['Authorization'] = 'Basic' + h2c.userName + ":" + h2c.password;
 
         h2c.doSomething = function() {
             alert(h2c.userName + ' : ' + h2c.password + " : " + h2c.subdomain);
+            $http.defaults.headers.common['Authorization'] = 'Basic' + h2c.userName + ":" + h2c.password;
 
-            var encoded = Base64.encode(h2c.username + ':' + h2c.password);
-            $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
 
             var successCallback = function(res) {
                 alert(res.data.records);
